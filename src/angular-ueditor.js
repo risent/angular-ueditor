3e62434a9e2efda14fc65685f7aa85209b349aa7
Risent Zhang <shengqi542@gmail.com>
2014-12-02 09:47:02 +0800

    Change ueditor instance initialize .

--- src/angular-ueditor.js
+++ src/angular-ueditor.js
@@ -5,24 +5,25 @@ angular.module('risent.ueditor',[]).
             restrict: 'A',
             require: 'ngModel',
             link: function (scope, element, attrs, ngModel) {
-                var eleId = element.attr('id');
-                if (eleId == undefined) {
-                    window.UEDITOR_ID_SEQ = window.UEDITOR_ID_SEQ || 0;
-                    eleId = 'ueditor-' + window.UEDITOR_ID_SEQ;
-                    element.attr('id', eleId);
-                    window.UEDITOR_ID_SEQ += 1;
-                }
-                var editor = UE.getEditor(eleId);
+                var editor = new UE.ui.Editor();
+                editor.render(element[0]);
+
+                /* handle when ngModel is assign from promise */
                 var watchDog = true;
-                scope.$watch(function(){return ngModel.$modelValue}, function(modelValue){
-                    if (modelValue && watchDog) {
+                scope.$watch(function () {
+                    return ngModel.$modelValue
+                }, function (modelValue) {
+                    if (modelValue && watchDog && !ngModel.$dirty) {
                         editor.setContent(modelValue);
                         watchDog = false;
                     }
                 })
-                editor.on('contentChange', function() {
+
+                editor.on('contentChange', function () {
                     element.val(editor.getContent());
                     ngModel.$setViewValue(editor.getContent());
+                    if (!scope.$root.$$phase)
+                        scope.$apply();
                 })
             }
         }

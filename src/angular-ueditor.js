5720749dfb6172d537b1c9852c0e067c2633ea95
Risent Zhang <shengqi542@gmail.com>
2014-12-02 22:28:35 +0800

    Fixed editor width when textarea transform from hide to display.

--- src/angular-ueditor.js
+++ src/angular-ueditor.js
@@ -14,10 +14,40 @@ angular.module('risent.ueditor',[]).
                     return ngModel.$modelValue
                 }, function (modelValue) {
                     if (modelValue && watchDog && !ngModel.$dirty) {
-                        editor.setContent(modelValue);
-                        watchDog = false;
+                        resizeEditor(editor, function(){
+                            watchDog = false;
+                        })
                     }
                 })
+
+                scope.$watch(function () {
+                    return element.parent().is(':visible');
+                }, function (isVisible) {
+                    if (isVisible) {
+                        $(editor.container).width('100%');
+                        $(editor.container).find('.edui-editor-iframeholder').width('100%');
+
+                        resizeEditor(editor);
+                    }
+                })
+
+                function resizeEditor(editor, fn) {
+                    editor.ready(function(){
+                        if (ngModel.$modelValue) {
+                            editor.setContent(ngModel.$modelValue);
+                        }
+
+                        // Set the editor min height to 260px
+                        if($(editor.container).height() > 260) {
+                            editor.setHeight('100%');
+                        } else {
+                            editor.setHeight(260);
+                        }
+                        if (fn) {
+                            fn.call(editor);
+                        }
+                    })
+                }
 
                 editor.on('contentChange', function () {
                     element.val(editor.getContent());
